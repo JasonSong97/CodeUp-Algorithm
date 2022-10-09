5d7e6a7429d667c45596918a4b56014a33323766
JasonSong97 <jaegeunsong97@gmail.com>
2022-10-09 21:14:23 +0900

    codeup 6078
    codeup 6078

---
+++ codeup6078/codeup6078.py
@@ -0,0 +1,14 @@
+# 모범답안
+while True:
+    x=input()
+    print(x)
+    if x=='q':
+        break
+
+# 1
+while True:
+    x = input()
+    if x == 'q':
+        print('q')
+        break
+    print(x)
