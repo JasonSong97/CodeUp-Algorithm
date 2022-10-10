237ab86dd91178957b3e2d3ac5f09f30b06bb4c2
JasonSong97 <jaegeunsong97@gmail.com>
2022-10-10 22:49:56 +0900

    codeup 6077
    codeup 6077

---
+++ codeup6077/codeup6077.py
@@ -0,0 +1,17 @@
+# 모범답안
+n = int(input())
+
+sum=0
+for i in range(1, n+1):
+    if i%2==0:
+        sum=sum+i
+
+print(sum)
+
+# 1
+x = int(input())
+sum = 0
+for i in range(1, x+1):
+    if i % 2 == 0:
+       sum += i 
+print(sum)
