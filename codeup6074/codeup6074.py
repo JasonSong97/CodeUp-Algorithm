e88e91a77f4950d2df199a1c54cb9e2f7685cb51
JasonSong97 <jaegeunsong97@gmail.com>
2022-10-13 11:35:50 +0900

    codeup 6074
    codeup 6074

---
+++ codeup6074/codeup6074.py
@@ -0,0 +1,15 @@
+# 모범답안
+c=input()
+i = ord('a')
+c = ord(c)
+
+while i<=c:
+    print(chr(i), end=' ')
+    i+=1
+
+# 1
+x = ord(input())
+A = ord('a')
+while A <= x :
+    print(chr(A), end=' ')
+    A += 1
