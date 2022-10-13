30fa8d2f92f020aa6ca8c4c566643668492079c6
JasonSong97 <jaegeunsong97@gmail.com>
2022-10-13 11:37:08 +0900

    codeup 6070
    codeup 6070

---
+++ codeup6070/codeup6070.py
@@ -0,0 +1,22 @@
+# 모범답안
+a=int(input())
+if a//3==1:
+    print("spring")
+elif a//3==2:
+    print("summer")
+elif a//3==3:
+    print("fall")
+else:
+    print("winter")
+
+# 1
+x = int(input())
+
+if x // 3 == 1:
+    print('spring')
+elif x == 6 or x == 7 or x == 8:
+    print('summer')
+elif x == 9 or x == 10 or x == 11:
+    print('fall')
+else:
+    print('winter')
