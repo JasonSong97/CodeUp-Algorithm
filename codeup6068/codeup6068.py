0b00e2683c561b5abe7876821b24e45321325351
JasonSong97 <jaegeunsong97@gmail.com>
2022-10-13 11:35:07 +0900

    codeup 6068
    codeup 6068

---
+++ codeup6068/codeup6068.py
@@ -0,0 +1,19 @@
+# 모범답안
+a=int(input())
+
+if a>=90:
+    print("A")
+elif a>=70:
+    print("B")
+elif a>=40:
+    print("C")
+else:
+    print("D")
+
+# 1
+x = int(input())
+
+if 90 <= x <= 100: print('A')
+elif 70 <= x <= 89: print('B')
+elif 40 <= x <= 69: print('C')
+elif 0 <= x <= 39: print('D')
