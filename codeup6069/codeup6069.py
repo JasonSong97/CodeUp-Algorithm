264b35d8da965685ee080266c21e1c843252e773
JasonSong97 <jaegeunsong97@gmail.com>
2022-10-13 11:37:01 +0900

    codeup 6069
    codeup 6069

---
+++ codeup6069/codeup6069.py
@@ -0,0 +1,21 @@
+# 모범답안
+a=input()
+
+if a=='A':
+    print("best!!!")
+elif a=='B':
+    print("good!!")
+elif a=='C':
+    print("run!")
+elif a=='D':
+    print("slowly~")
+else:
+    print("what?")  
+
+# 1
+a = input()
+if a == 'A': print('best!!!')
+elif a == 'B': print('good!!')
+elif a == 'C': print('run!')
+elif a == 'D': print('slowly~')
+else: print('what?')
