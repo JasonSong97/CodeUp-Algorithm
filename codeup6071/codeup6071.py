c8128e4f50172b11ae5d70341109cbcf49b25072
JasonSong97 <jaegeunsong97@gmail.com>
2022-10-13 11:37:15 +0900

    codeup 6071
    codeup 6071

---
+++ codeup6071/codeup6071.py
@@ -0,0 +1,24 @@
+# 모범답안
+while True:
+    a=input()
+    a=int(a)
+    if a==0:
+        break
+    else:
+        print(a)
+
+# 1
+n = 1
+while n != 0:
+    n = int(input())
+    if n != 0:
+        print(n)
+
+# 2
+n = 1
+while n != 0:
+    n = int(input())
+    if n !=0:
+        print(n)
+    else:
+        break
