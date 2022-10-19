6018ab274991b26fbacbf496f077a502e4c53a44
JasonSong97 <jaegeunsong97@gmail.com>
2022-10-19 22:28:15 +0900

    codeup 6044
    codeup 6044

---
+++ codeup6044/codeup6044.py
@@ -0,0 +1,19 @@
+# 모범답안
+a,b=input().split()
+a=int(a)
+b=int(b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(round(a/b,2))
+
+# 1
+a, b = map(int, input().split())
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
+print(format(a/b, '.2f'))
